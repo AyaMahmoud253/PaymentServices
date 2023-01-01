a597a5f60d3fbc52bffda253297962e2fd1bb2ae
HagarSami <hagar.hs13@gmail.com>
2023-01-01 15:31:10 +0200

    removed

--- SE_Project/src/main/java/com/uniProject/SE_Project/serviceProvider/Provider.java
+++
@@ -1,6 +0,0 @@
-package com.uniProject.SE_Project.serviceProvider;
-
-import org.springframework.data.repository.CrudRepository;
-
-/*public interface Provider extends CrudRepository<ServiceProviderEntity,String> {
-}*/
